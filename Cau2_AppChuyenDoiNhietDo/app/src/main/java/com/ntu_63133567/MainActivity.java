5f18c55850486216e8fd495ae86f6fbd69050d25
PhungHoanChien <hoanchien.04012003@gmail.com>
2024-03-25 11:57:42 +0700

    push

--- Cau2_AppChuyenDoiNhietDo/app/src/main/java/com/ntu_63133567/MainActivity.java
+++ Cau2_AppChuyenDoiNhietDo/app/src/main/java/com/ntu_63133567/MainActivity.java
@@ -1,6 +1,9 @@
 package com.ntu_63133567;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,8 +13,12 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edtF, edtC;
+    Button btnCF, btnFC , btnclear;
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
@@ -19,6 +26,15 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+
         });
+        edtC = findViewById(R.id.edtC);
+        edtF = findViewById(R.id.edtF);
+        btnCF = findViewById(R.id.btnCF);
+        btnFC = findViewById(R.id.btnFC);
+        btnclear = findViewById(R.id.btnclear);
+
+        btnCF
     }
 }
