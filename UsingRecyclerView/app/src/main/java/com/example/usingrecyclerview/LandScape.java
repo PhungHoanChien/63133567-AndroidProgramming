5b300b1dd5961b1b0d133eb665c20b9d1cae9465
PhungHoanChien <hoanchien.04012003@gmail.com>
2024-04-10 08:38:05 +0700

    push

---
+++ UsingRecyclerView/app/src/main/java/com/example/usingrecyclerview/LandScape.java
@@ -0,0 +1,27 @@
+package com.example.usingrecyclerview;
+
+public class LandScape {
+    String landImageName;
+    String landCation;
+
+    public LandScape(String landImageName, String landCation) {
+        this.landImageName = landImageName;
+        this.landCation = landCation;
+    }
+
+    public String getLandImageName() {
+        return landImageName;
+    }
+
+    public void setLandImageName(String landImageName) {
+        this.landImageName = landImageName;
+    }
+
+    public String getLandCation() {
+        return landCation;
+    }
+
+    public void setLandCation(String landCation) {
+        this.landCation = landCation;
+    }
+}
---
+++ UsingRecyclerView/app/src/main/java/com/example/usingrecyclerview/LandScapeAdapter.java
@@ -0,0 +1,72 @@
+package com.example.usingrecyclerview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class LandScapeAdapter extends RecyclerView.Adapter<LandScapeAdapter.ItemLandHolder> {
+    Context context;
+    ArrayList<LandScape> lstData;
+
+    public LandScapeAdapter(Context context, ArrayList<LandScape> lstData) {
+        this.context = context;
+        this.lstData = lstData;
+    }
+
+    @NonNull
+    @Override
+    public ItemLandHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater cai_bom = LayoutInflater.from(context);
+        View vItem =  cai_bom.inflate(R.layout.item_land,parent,false);
+        ItemLandHolder viewholderCreated = new ItemLandHolder(vItem);
+        return viewholderCreated;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemLandHolder holder, int position) {
+        LandScape landScapeHienThi = lstData.get(position);
+        String caption = landScapeHienThi.getLandCation();
+        String tenFileAnh = landScapeHienThi.getLandImageName();
+        holder.tvCaption.setText(caption);
+        String packageName = holder.itemView.getContext().getPackageName();
+        int imageID = holder.itemView.getResources().getIdentifier(tenFileAnh,"mipmap",packageName);
+        holder.ivLandscape.setImageResource(imageID);
+    }
+
+    @Override
+    public int getItemCount() {
+        return lstData.size() ;
+    }
+
+    class ItemLandHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        TextView tvCaption;
+        ImageView ivLandscape;
+
+        public ItemLandHolder(@NonNull View itemView) {
+            super(itemView);
+            tvCaption = itemView.findViewById(R.id.textViewCation);
+            ivLandscape = itemView.findViewById(R.id.imageViewLand);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int viTriDuocClick = getAdapterPosition();
+            LandScape phanTuDuocClick = lstData.get(viTriDuocClick);
+            String ten = phanTuDuocClick.getLandCation();
+            String tenFile = phanTuDuocClick.getLandImageName();
+            String chuoiThongBao = "Bạn vừa click vào: "+ten;
+            Toast.makeText(v.getContext(),chuoiThongBao,Toast.LENGTH_SHORT).show();
+        }
+    }
+}
